936eee5099bacc0152f937e6b59dadedf617274d
Demirkol, Goerkem <gdemirkol315@gmail.com>
2022-03-23 00:02:18 +0100

    CORS Policy update, enhanced server.js, made basic example to get data from back to front

--- backend/app.js
+++ backend/app.js
@@ -3,14 +3,23 @@ const express = require('express');
 
 const app = express();
 
-app.use((req,res,next)=>{
-  console.log('first middleware');
-  next(); //always call next if you are not sending a response
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Request-With, Content-Type, Accept");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE, OPTIONS");
+  next();
+})
+
+app.use('/api/posts', (req, res, next) => {
+  const posts = [{id: 'dsadas', title: 'bu bir title', text: 'patates'}];
+
+  res.status(200).json({
+    message: 'Posts fetched successfully!',
+    posts: posts
+  });
 });
 
-app.use((req,res,next)=>{
-
-  res.send('Hello from express!')
-});
 
 module.exports = app;
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { PostComponent } from './post/post.component';
+import {HttpClientModule} from "@angular/common/http";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PostComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
