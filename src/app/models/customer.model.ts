e920366e2ae366b40b0b8d33647650be89efbd17
Demirkol, Goerkem <gdemirkol315@gmail.com>
2022-05-08 09:14:33 +0200

    CU-2a25rbm: customer model added

---
+++ src/app/models/customer.model.ts
@@ -0,0 +1,15 @@
+
+export class Customer {
+  name:string;
+  lastname:string;
+  type:string;
+
+
+  constructor(name: string, lastname: string, type: string) {
+    this.name = name;
+    this.lastname = lastname;
+    this.type = type;
+  }
+}
+
+
